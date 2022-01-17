a1c49eb0ad03637a9b12aba0587baf312d45b9eb
MaycoH <64017627+MaycoH@users.noreply.github.com>
2022-01-17 22:58:21 +0100

    Úprava chybových hlášok v závislosti od vrátenej výnimky

--- App/Views/Home/TablesNotCreated.view.php
+++ App/Views/Home/TablesNotCreated.view.php
@@ -4,24 +4,30 @@
 <div class="container">
     <div class="row">
         <div class="col col-md-8">
-            <h3 style="color:red">Všetky tabuľky neboli ešte vytvorené!</h3>
             <?php
-            $count = \App\Core\DB\Connection::connect()->prepare("SHOW TABLES");    // Pomocou "connect()" si vyžiadam spojenie a preparenem si SQL
-            $count->execute([]);    // Spustím ho
-            $count2 = $count->fetchAll();
-
-            $tableNames = ["aktuality", "comments", "events", "users"];
-            $i = 0;
-            foreach ($count2 as $item) {
-                if ($item[0] != $tableNames[$i]) {
-                    ?>
-                    Tabuľka <strong>"<?php echo $tableNames[$i] ?>"</strong> ešte nebola vytvorená!<br>
-                    <?php
-                    $i++;
-                } else $i++;
-            }
-            ?>
-
+            try {
+                $count = \App\Core\DB\Connection::connect()->prepare("SHOW TABLES");
+                $count->execute([]);    // Spustím ho
+                $count2 = $count->fetchAll(); ?>
+                <h3 class="register-error">Všetky tabuľky neboli ešte vytvorené!</h3>
+                <?php
+                $tableNames = ["aktuality", "comments", "events", "users"];
+                $i = 0;
+                foreach ($count2 as $item) {
+                    if ($item[0] != $tableNames[$i]) {
+                        ?>
+                        Tabuľka <strong>"<?php echo $tableNames[$i] ?>"</strong> ešte nebola vytvorená!<br>
+                        <?php
+                        $i++;
+                    } else $i++;
+                }
+            } catch (Exception $e) {
+                if (str_contains($e->getMessage(), '[2002]')) {     // Zlyhalo pripojenie ?>
+                    <h3 class="register-error">Problém s pripojením k DB! Je databázový server spustený?</h3>
+                <?php } else if (str_contains($e->getMessage(), '[1049]')) {    // Nesprávna databáza ?>
+                    <h3 class="register-error">Nesprávna schéma databázy! Skontrolujte, či je zvolená správna databáza.</h3>
+                <?php }
+            } ?>
         </div>
     </div>
 </div>
